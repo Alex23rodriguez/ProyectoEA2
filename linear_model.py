cadec7a42c85a78ed329b19e538c0df14dffa480
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2021-05-22 10:51:20 -0500

    fixed a couple of bugs

--- linear_model.py
+++ linear_model.py
@@ -1,6 +1,8 @@
+from os import stat
 from numpy.core.numeric import identity
 from scipy import matrix, stats
 import numpy as np
+import pandas as pd
 
 
 class LM:
@@ -15,21 +17,27 @@ class LM:
         # set self.explain, self.p, self.H and self.X
         self.update(self.cols.copy())
 
+    @staticmethod
+    def from_matrix(X, y):
+        ydf = pd.DataFrame({'y': y})
+        xdf = pd.DataFrame(X)
+        return LM(ydf.join(xdf), 'y')
+
     def predict(self, x):
         pass
 
     def update(self, explain):
         assert all([x in self.cols for x in explain])
         self.explain = explain
-        self.p = len(self.explain)
+        self.p = len(self.explain) + 1
         self.X = matrix(self.df[self.explain])
         self.X = np.concatenate(
             [matrix(np.ones(len(self.X))).T, self.X], axis=1)
 
         self.H = self.X*((self.X.T*self.X)**-1)*self.X.T
         self.beta_hat = self._get_beta_hat()
-        self.var_beta_hat = self._get_var_beta_hat()
         self.sigma_sq_hat = self._get_sigma_sq_hat()
+        self.var_beta_hat = self._get_var_beta_hat()
 
     def _get_beta_hat(self):
         return (self.X.T*self.X)**-1 * self.X.T * self.y
@@ -41,7 +49,7 @@ class LM:
         return self.X*((self.X.T*self.X)**-1) * self.X.T * self.y
 
     def _get_sigma_sq_hat(self):
-        return 1/(self.n-self.p)*(self.y.T*(np.identity(self.n) - self.H)) * self.y
+        return 1/(self.n-self.p)*(self.y.T*(np.identity(self.n) - self.H) * self.y)[0, 0]
 
     def _get_cme(self):
         return (1/(self.n - self.p))*(self.y.T*(np.identity(self.n) - self.H)*self.y)
@@ -66,8 +74,10 @@ class LM:
 
     def IC_beta_hat(self, j, alpha):
         tval = stats.t.ppf(alpha/2, self.n-self.p)
-        k = tval * (self.sigma_sq_hat * self.var_beta_hat[j][j])**0.5
-        return self.beta_hat[j] - k, self.beta_hat[j] + k
+        k = tval * (self.sigma_sq_hat * self.var_beta_hat[j, j])**0.5
+        print(self.beta_hat[j, 0], tval,
+              self.sigma_sq_hat, self.var_beta_hat[j, j])
+        return self.beta_hat[j, 0] - k, self.beta_hat[j, 0] + k
 
     def _get_R_ajustado(self):
         return 1 - (self._get_cme()/self._get_cmr())
