27dae12f087de53351cc52cb4cfa65fdf0e4268e
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2021-05-22 14:32:35 -0500

    initial tests on dataset

---
+++ project.py
@@ -0,0 +1,20 @@
+# %%
+from linear_model import LM
+from matplotlib import pyplot
+import linear_model
+from importlib import reload
+reload(linear_model)
+
+# %%
+df = pd.read_csv('airfoil_self_noise.dat', sep='\t')
+df.columns = ['freq', 'angle', 'chord_length',
+              'free_stream_vel', 'displacement_thickness', 'decibels']
+lm = LM(df, 'decibels')
+
+# %%
+r = lm.get_residuals()
+# %%
+pyplot.boxplot(r)
+# %%
+lm.residual_summary()
+# %%
